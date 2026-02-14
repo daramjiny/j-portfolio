cdfaceb7265f98b5b879aaeb2da3aaf5ff8f868e
daramjiny <86844788+daramjiny@users.noreply.github.com>
2026-02-14 19:05:01 +0900

    Fix Instagram Slider background size and margin

--- src/app/pages/ProjectDetail.tsx
+++ src/app/pages/ProjectDetail.tsx
@@ -55,7 +55,7 @@ export function ProjectDetail() {
                             <LockScreenSlider />
                         </div>
                     ) : project.id === "emart-mall-face" ? (
-                        <div className="relative w-full py-12 lg:pt-24 flex items-center lg:items-start justify-center overflow-hidden h-auto lg:h-screen lg:sticky lg:top-0">
+                        <div className="relative w-full h-auto lg:h-screen lg:sticky lg:top-0">
                             <InstagramFeedSlider />
                         </div>
                     ) : (
